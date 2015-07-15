109c76fce8f1f3cba0ebf920edea49a861997806
Tyler Kilbourne <tyk@asu.edu>
2015-07-15 16:25:01 -0700

    updated css styles in panel layout

--- plugins/layouts/cherokee/cherokee.tpl.php
+++ plugins/layouts/cherokee/cherokee.tpl.php
@@ -10,74 +10,91 @@
  */
 ?>
 
-<div class="panel-display cherokee clearfix container <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="cherokee-container cherokee-header clearfix panel-panel row-fluid">
-    <div class="cherokee-container-inner cherokee-header-inner panel-panel-inner span12">
+<div class="panel-display cherokee clearfix <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 	
-      <?php print $content['header']; ?>
-	  
-    </div>
-  </div>
+<section class="section">
+    <div class="container">
+      <div class="row row-1 cherokee-header row-full">
+        <div class="column col-md-12">
+          
+		  <?php print $content['header']; ?>
+		  
+        </div>
+      </div>
+    </div><!-- /.container -->
+</section><!-- /.section-post-header -->
 
+<section class="section">
+  <div class="container">
   
-  <div class="cherokee-container cherokee-top clearfix row-fluid">
-    <div class="cherokee-top-region cherokee-slider panel-panel span8">
-      <div class="cherokee-top-region-inner cherokee-slider-inner panel-panel-inner">
+	  <div class="cherokee-container cherokee-top clearfix row">
+		<div class="cherokee-top-region cherokee-slider panel-panel column col-sm-8">
+		  <div class="cherokee-top-region-inner cherokee-slider-inner panel-panel-inner">
+		  
+			<?php print $content['slider']; ?>
+			
+		  </div>
+		</div>
+		<div class="cherokee-top-region cherokee-slider-gutter panel-panel column col-sm-4">
+		  <div class="cherokee-top-region-inner cherokee-slider-gutter-inner panel-panel-inner">
+		  
+			<?php print $content['slidergutter']; ?>
+			
+		  </div>
+		</div>
+	  </div>
 	  
-        <?php print $content['slider']; ?>
-		
-      </div>
-    </div>
-    <div class="cherokee-top-region cherokee-slider-gutter panel-panel span4">
-      <div class="cherokee-top-region-inner cherokee-slider-gutter-inner panel-panel-inner">
-	  
-        <?php print $content['slidergutter']; ?>
-		
-      </div>
-    </div>
-  </div>
 
-  
-  <div class="cherokee-container cherokee-center clearfix panel-panel row-fluid">
-    <div class="cherokee-container-inner cherokee-center-inner panel-panel-inner span12">
-	
-      <?php print $content['center']; ?>
 	  
-    </div>
-  </div>
-  
-  
-  <div class="cherokee-container cherokee-middle clearfix row-fluid">
-    <div class="cherokee-middle-region cherokee-column1 panel-panel span4">
-      <div class="cherokee-middle-region-inner cherokee-column1-inner panel-panel-inner">
-	  
-        <?php print $content['column1']; ?>
+	  <div class="cherokee-container cherokee-center clearfix panel-panel row">
+		<div class="cherokee-container-inner cherokee-center-inner panel-panel-inner column col-md-12">
 		
-      </div>
-    </div>
-    <div class="cherokee-middle-region cherokee-column2 panel-panel span4">
-      <div class="cherokee-middle-region-inner cherokee-column2-inner panel-panel-inner">
-	  
-        <?php print $content['column2']; ?>
-		
-      </div>
-    </div>
-    <div class="cherokee-middle-region cherokee-column3 panel-panel span4">
-      <div class="cherokee-middle-region-inner cherokee-column3-inner panel-panel-inner">
-	  
-        <?php print $content['column3']; ?>
-		
-      </div>
-    </div>
-  </div>
+		  <?php print $content['center']; ?>
+		  
+		</div>
+	  </div>
 
-  <div class="cherokee-container cherokee-footer clearfix panel-panel row-fluid">
-    <div class="cherokee-container-inner cherokee-footer-inner panel-panel-inner span12">
-	
-      <?php print $content['footer']; ?>
 	  
-    </div>
+		<div class="cherokee-container cherokee-middle clearfix row">
+			<div class="cherokee-middle-region cherokee-column1 panel-panel column col-sm-4">
+				<div class="cherokee-middle-region-inner cherokee-column1-inner panel-panel-inner">
+
+				<?php print $content['column1']; ?>
+
+				</div>
+			</div>
+			<div class="cherokee-middle-region cherokee-column2 panel-panel column col-sm-4">
+				<div class="cherokee-middle-region-inner cherokee-column2-inner panel-panel-inner">
+
+				<?php print $content['column2']; ?>
+
+				</div>
+			</div>
+			<div class="cherokee-middle-region cherokee-column3 panel-panel column col-sm-4">
+				<div class="cherokee-middle-region-inner cherokee-column3-inner panel-panel-inner">
+
+				<?php print $content['column3']; ?>
+
+				</div>
+			</div>
+		</div>
+	  
+	  
+  
   </div>
+</section>
+  
+  	<section class="section">
+    <div class="container">
+      <div class="row row-1 cherokee-footer row-full">
+        <div class="column col-md-12">
+          
+		  <?php print $content['footer']; ?>
+		  
+        </div>
+      </div>
+    </div><!-- /.container -->
+  </section>
+
   
 </div><!-- /.cherokee -->
