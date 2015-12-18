d8f73d93fc83c1f63bc964dc4e31f11b0bc37e0e
Ryan J. Bonnell <ryan@fervorcreative.com>
2015-12-17 17:27:48 -0700

    Add Views template for Research Areas Vertical List Subcategories

---
+++ asu_clas_research_areas/templates/views-view-fields--research-area-list-vertical-subcategories-pane.tpl.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<div class="row research-area-term">
+  <div class="col-md-4">
+    <?php if (!empty($fields['research_area_image']->content)): ?>
+      <?php print $fields['research_area_image']->content; ?>
+    <?php else: ?>
+      <?php print theme_image(array('path' => drupal_get_path('module', 'asu_clas_research_areas') . '/images/placeholder-200x200.png', 'width' => 200, 'height' => 200)); ?>
+    <?php endif; ?>
+  </div>
+
+  <div class="col-md-8">
+    <?php if (isset($fields['name'])): ?>
+      <?php print $fields['name']->content; ?>
+    <?php endif; ?>
+
+    <?php if (isset($fields['description'])): ?>
+      <?php print $fields['description']->content; ?>
+    <?php endif; ?>
+  </div>
+</div>
