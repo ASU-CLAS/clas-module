9408f9882350b2e5c055353b2e1542e08e27e372
Ryan J. Bonnell <ryan@fervorcreative.com>
2016-02-08 17:35:54 -0700

    Replace Webspark Megamenu module inline JavaScript with our own logic

--- asu_clas_shesc_tb_megamenu/js/asu-clas-shesc-tb-megamenu.js
+++ asu_clas_shesc_tb_megamenu/js/asu-clas-shesc-tb-megamenu.js
@@ -1,21 +1,17 @@
 (function($, Drupal) {
-  Drupal.behaviors.webspark_megamenu = {
+  "use strict";
+
+  // Modify ASU Webspark Main Menu "Home" Link
+  Drupal.behaviors.asu_clas_shesc_tb_megamenu = {
     attach: function() {
-      $(".tb-megamenu-nav>li:first-child").remove();
+      var $menu_item = $('.tb-megamenu-main-menu').find('li:first');
+      var $menu_link = $menu_item.children('a');
 
-      var h = window.location.protocol+"//"+window.location.hostname+window.location.pathname;
-      h = h.replace(/\/$/,"");
-      var u = window.location.protocol+"//"+window.location.hostname;
-      var a = null;
-      var url = "'.variable_get('site_frontpage', 'node').'";
+      $menu_link.wrapInner('<i class="fa fa-home icon-white"><span style="display: none;"></span></i>');
 
-      if (h == u || h == u+"/"+url) {
-        a = "active";
+      if ($menu_link.attr('href') == Drupal.settings.basePath) {
+        $menu_link.addClass('active');
       }
-
-      var b = \'<li data-id="1092" data-level="1" data-type="menu_item" data-class="" data-xicon="" data-caption="" data-alignsub="" data-group="0" data-hidewcol="0" data-hidesub="0" class="tb-megamenu-item level-1 mega \'+a+\'"><a href="\'+u+\'?home" alt="Home"><i class="fa fa-home icon-white"><span style="display:none">Home</span></i></a></li>\';
-
-      $(".tb-megamenu-nav").prepend(b);
     }
   }
 }(jQuery, Drupal));
